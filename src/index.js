767cf5d6115c802e8d78c65f6729a94358f50ccc
boyko1989 <hunwiss89@gmail.com>
2023-01-16 18:09:54 +0400

    Начальное состояние проекта
    В данный момент имеем веб-приложение.
    Но возникает вопрос:
    Как добавить определённый абстрактный слой,
    позволяющий работать с данными и отделить
    его от представления?

--- src/index.js
+++ src/index.js
@@ -1,1 +1,47 @@
-import './styles.css'
+import "./styles/styles.css";
+
+const counter = document.getElementById("counter");
+const addBtn = document.getElementById("add");
+const subBtn = document.getElementById("sub");
+const asyncBtn = document.getElementById("async");
+const themeBtn = document.getElementById("theme");
+
+let state = 0;
+
+function render() {
+  counter.textContent = state.toString();
+}
+
+addBtn.addEventListener("click", () => {
+  state++;
+  render();
+});
+
+subBtn.addEventListener("click", () => {
+  state--;
+  render();
+});
+
+
+// addBtn.addEventListener("click", function () {
+//   state++;
+//   render();
+// });
+//
+// subBtn.addEventListener("click", function () {
+//   state--;
+//   render();
+// });
+
+asyncBtn.addEventListener("click", function () {
+  setTimeout(() => {
+    state++;
+    render();
+  }, 3000);
+});
+
+themeBtn.addEventListener("click", function () {
+  document.body.classList.toggle("dark");
+});
+
+render();
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,56 @@
 const path = require('path')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const FileManagerPlugin = require('filemanager-webpack-plugin')
 
 module.exports = {
   mode: 'development',
   context: path.resolve(__dirname, 'src'),
-  entry: './index.js',
+  entry: {
+    main: [
+      path.join(__dirname, 'src', 'index'),
+    ],
+    // modules: [],
+  },
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.join(__dirname, 'dist'),
+    filename: 'index.[contenthash].js'
+  },
+  devServer: {
+    watchFiles: path.join(__dirname, 'src'),
+    port: 4220,
+    hot: true,
+    compress: true
   },
   plugins: [
     new HTMLWebpackPlugin({
       template: 'index.html'
-    })
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin ({
+      filename: '[name].[contenthash].css',
+    }),
+    new FileManagerPlugin({
+      events: {
+        onStart: {
+          delete: ['dist/*']
+        },
+        // onEnd: {},
+      }
+    }),
   ],
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        exclude: /node_modules/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+        ],
       }
     ]
-  },
-  devServer: {
-    port: 3000
   }
 }
