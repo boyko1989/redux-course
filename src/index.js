97a3c1895cbb4a44339346a28da236deaa09a2a5
boyko1989 <hunwiss89@gmail.com>
2023-01-16 21:41:52 +0400

    Добавлена обработка асинхронности

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
 import {rootReducer} from "./redux/rootReducer";
-import {increment, decrement} from "./redux/actions";
+import {increment, decrement, asyncIncrement} from "./redux/actions";
 import "./styles/styles.css";
 
 const counter = document.getElementById("counter");
@@ -9,7 +10,11 @@ const subBtn = document.getElementById("sub");
 const asyncBtn = document.getElementById("async");
 const themeBtn = document.getElementById("theme");
 
-const store = createStore(rootReducer, 0)
+const store = createStore (
+    rootReducer,
+    0,
+    applyMiddleware(thunk)
+)
 
 addBtn.addEventListener("click", () => {
     store.dispatch(increment())
@@ -20,12 +25,11 @@ subBtn.addEventListener("click", () => {
 });
 
 asyncBtn.addEventListener("click", function () {
-
+    store.dispatch(asyncIncrement ())
 });
 
 store.subscribe(() => {
     const state = store.getState()
-
     counter.textContent = state
 })
 
